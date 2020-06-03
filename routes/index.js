9ad1b0070b56a60a104aa112879ca108902a5244
PierreVelluet <velluetp@gmail.com>
2020-06-03 16:56:32 +0200

    job done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const clients = require('./clients')
+const tags = require('./tags')
+const products = require('./products')
 
 const router = express.Router();
 
 router.use('/clients', clients)
+router.use('/products', products)
+router.use('/tags', tags)
 
 module.exports = router;
