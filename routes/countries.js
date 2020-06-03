27ba30da2f84621bfb73293021e9762fe7cf7dd0
mathildetho <mathilde.thoraninth@gmail.com>
2020-06-03 17:17:19 +0200

    create get and put country

--- routes/countries.js
+++ routes/countries.js
@@ -25,4 +25,36 @@ router.post('/', (req, res) => {
     })
 })
 
+//PUT a country with it's ID
+router.put('/:id', (req, res) => {
+    const body = req.body
+    const id = req.params.id
+    connection.query('UPDATE country SET ? WHERE id = ?', [body, id], (err, result) => {
+        if (err) {
+            res.status(500).json('Erreur lors de la modification d\'un pays')
+        } else {
+            connection.query('SELECT * FROM country WHERE id = ?', id, (err2, result2) => {
+                if (err) {
+                    res.status(500).json('Erreur lors de la récupération du client')
+                } else {
+                    res.status(200).json(result2)
+                }
+            })
+        }
+    })
+});
+
+//DELETE a country with it's ID.
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    connection.query('DELETE FROM country WHERE id = ?', id, (err, result) => {
+        if (err) {
+            res.status(500).json('Erreur lors de la supression d\'un pays')
+        } else {
+            res.status(200).json('Pays supprimé')
+        }
+    })
+})
+
+
 module.exports = router;
