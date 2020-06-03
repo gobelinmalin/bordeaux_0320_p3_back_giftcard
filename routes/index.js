29bc9d55e7b9d154d46e6ed0d40c7ef2c4ac2582
mathildetho <mathilde.thoraninth@gmail.com>
2020-06-03 12:08:56 +0200

    create routes customizations get, post, put, delete

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const clients = require('./clients')
+const clients = require('./clients');
+const customizations = require('./customizations');
 
 const router = express.Router();
 
-router.use('/clients', clients)
+router.use('/clients', clients);
+router.use('/customizations', customizations);
 
 module.exports = router;
