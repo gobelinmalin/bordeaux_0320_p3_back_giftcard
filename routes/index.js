e8d7df5cb30ab246c50e2f837c502ef0965f0105
mathildetho <mathilde.thoraninth@gmail.com>
2020-06-03 17:09:59 +0200

    done font

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const clients = require('./clients');
 const customizations = require('./customizations');
+const fonts = require('./fonts');
 
 const router = express.Router();
 
 router.use('/clients', clients);
 router.use('/customizations', customizations);
+router.use('/fonts', fonts);
 
 module.exports = router;
