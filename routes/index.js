79226d792e4919e8aea16968c5b27b361ef335f8
mathildetho <mathilde.thoraninth@gmail.com>
2020-06-03 11:30:23 +0200

    create crud order but pb bc don't have the joins

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const clients = require('./clients')
+const orders = require('./orders')
 
 const router = express.Router();
 
 router.use('/clients', clients)
+router.use('/orders', orders)
 
 module.exports = router;
---
+++ routes/orders.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const connection = require('../config');
+
+const router = express.Router();
+
+// get all orders
+router.get('/', (req, res) => {
+    connection.query('SELECT * FROM order', (err, results) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Erreur lors de la récupération des commandes');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+// get one order
+router.get('/:id', (req, res) => {
+    const oneOrder = req.params.id;
+    connection.query('SELECT * FROM order WHERE id = ?', [oneOrder], (err, results) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send(`Erreur lors de la récupération de la commande ${oneOrder}`);
+        } 
+        if (results.length === 0) {
+            res.status(404).send('Commande non trouvée')
+        } else {
+            res.json(results[0]);
+        }
+    });
+});
+
+// post one order
+router.post('/', (req, res) => {
+    const addOrder = req.body;
+    connection.query('INSERT INTO order SET ?', addOrder, (err, results) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Erreur lors de la sauvegarde de la commande');
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+// put one order
+router.put('/:id', (req, res) => {
+    const idOrder = req.params.id;
+    const formData = req.body;
+    connection.query('UPDATE order SET ? WHERE id = ?', [formData, idOrder], err => {
+        if(err) {
+            console.log(err);
+            res.status(500).send(`Erreur lors de la modification de la commande ${idOrder}`);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+// delete one order
+router.delete('/:id', (req, res) => {
+    const idOrder = req.params.id;
+    connection.query('DELETE FROM order WHERE id = ?', [idOrder], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Erreur lors de la suppression de la commande')
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+module.exports = router;
