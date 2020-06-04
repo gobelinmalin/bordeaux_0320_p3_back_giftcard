6c1e85624b61ca7408860181d1fce957911c0ac7
PierreVelluet <velluetp@gmail.com>
2020-06-04 15:18:42 +0200

    favorite-product

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const tags = require('./tags');
 const products = require('./products');
 const customizations = require('./customizations');
 const categories = require('./categories');
-const favoriteProduct = require('./favoriteProduct');
 
 const router = express.Router();
 
@@ -15,7 +14,6 @@ router.use('/products', products);
 router.use('/tags', tags);
 router.use('/customizations', customizations);
 router.use('/categories', categories);
-router.use('/favorites', favoriteProduct);
 
 
 
