b8b504e5481d53c0c33761a221eb2ef3093dae5f
koob <zeebha@gmail.com>
2020-07-30 09:19:21 +0200

    add express-validator + config index

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
+const fileUpload = require('express-fileupload');
+
 app.use(express.static("public"));
 
 app.use(cors());
@@ -19,3 +21,26 @@ app.listen(port, (err) => {
         throw new Error("There is an error");
     }
 });
+
+
+// UPLOAD FILE
+// to access the files in public folder
+app.use(express.static('public'));
+app.use(fileUpload());
+// file upload api
+app.post('api/upload', (req, res) => {
+  if (!req.files) {
+      return res.status(500).send({ msg: "file is not found" })
+  }
+  // accessing the file
+  const myFile = req.files.file;
+  // mv() method places the file inside public directory
+  myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
+      if (err) {
+          console.log(err)
+          return res.status(500).send({ msg: "Error occured" });
+      }
+      // return the response with file path and name
+      return res.send({name: myFile.name, path: `/${myFile.name}`});
+  });
+})
